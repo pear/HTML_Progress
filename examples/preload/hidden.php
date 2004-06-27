f70de7dbb0dd5e1103419c2ee23bbaf4f0ad28ec
Laurent Laville <farell@php.net>
2004-06-27 18:30:54 +0000

    example on new function HTML_Progress::hide() in version 1.2.0RC3
    git-svn-id: http://svn.php.net/repository/pear/packages/HTML_Progress/trunk@162183 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ examples/preload/hidden.php
@@ -0,0 +1,62 @@
+<?php
+@include '../include_path.php';
+/**
+ * Simple example that hide a progress meter at end of process.
+ *
+ * @version    $Id$
+ * @author     Laurent Laville <pear@laurent-laville.org>
+ * @package    HTML_Progress
+ */
+
+require_once 'HTML/Progress.php';
+
+/*
+    user callback: job to do while the progress meter is visible
+ */
+function myFunctionHandler($progressValue, &$obj)
+{
+    $obj->sleep();  // nothing to do here, except sleep a bit ...
+}
+
+$progress = new HTML_Progress();
+$progress->setAnimSpeed(100);
+$progress->setIncrement(10);
+$progress->setProgressHandler('myFunctionHandler');
+?>
+<html>
+<head>
+<style type="text/css">
+<!--
+body {
+	background-color: #CCCC99;
+	color: #996;
+	font-family: Verdana, Arial;
+}
+
+a:visited, a:active, a:link {
+	color: yellow;
+}
+
+<?php echo $progress->getStyle(); ?>
+// -->
+</style>
+<script type="text/javascript">
+<!--
+<?php echo $progress->getScript(); ?>
+//-->
+</script>
+</head>
+<body>
+
+<?php 
+echo $progress->toHtml();  
+$progress->run();
+$progress->hide();
+?>
+
+<h1>Your job is finished ! </h1>
+<p>The progress meter is now hidden.</p>
+<p>&lt;&lt; <a href="../index.html">Back examples TOC</a></p>
+
+</body>
+</html>
