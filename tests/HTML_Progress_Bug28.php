16f828c00e31f42d68cee40a5e60522a495b57d1
Laurent Laville <farell@php.net>
2004-11-01 16:51:55 +0000

    add unit test for bug#28
    git-svn-id: http://svn.php.net/repository/pear/packages/HTML_Progress/trunk@171850 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ tests/HTML_Progress_Bug28.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * BUG #28 regression test for HTML_Progress class.
+ * 
+ * @version    $Id$
+ * @author     Laurent Laville <pear@laurent-laville.org>
+ * @package    HTML_Progress
+ * @link       http://pear.php.net/bugs/bug.php?id=28
+ */
+
+error_reporting(E_ALL);
+
+function php_error_handler($errno, $errstr, $errfile, $errline)
+{
+    die("<b>myhandler</b> $errstr in $errfile at line $errline");
+}
+set_error_handler('php_error_handler');
+
+require_once 'HTML/Progress.php';
+$pkg = array('PEAR', 'Config');
+$bar = new HTML_Progress();
+$bar->setAnimSpeed(1000);
+$bar->setIncrement(50);
+?>
+<style type="text/css">
+<!--
+body {
+    background-color: lightgrey;
+    font-family: Verdana, Arial;
+}
+<?php echo $bar->getStyle(); ?>
+// -->
+</style>
+<script type="text/javascript">
+<!--
+<?php echo $bar->getScript(); ?>
+//-->
+</script>
+<?php
+echo $bar->toHtml();
+
+for ($i=0; $i<10; $i++) {
+    $bar->display();
+    echo "installing package ... : ". $pkg[$i] ."<br /> \n";
+    $bar->sleep();
+    $bar->incValue();
+}
+?>
